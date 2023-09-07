488536c99876c8356abc562967aaed278bba8132
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2023-09-07 08:51:03 -0500

    Tercer codigo Evaluacion

---
+++ Factorial.js
@@ -0,0 +1,9 @@
+var Numero = parseInt(prompt("De que numero quiere ver el factorial"))
+
+var count = 1;
+
+for (var i = 0; i>=Numero; i++){
+    var a = i - Numero
+    count = a * count
+}
+console.log(i)
