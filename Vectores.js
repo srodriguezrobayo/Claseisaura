14ea1c77b89516a993b5dc9cd22f7fc6acb89aa9
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2023-09-07 09:12:54 -0500

    Algoritmo 5 evalucion
    Ya perdi esta evaluacion

---
+++ Vectores.js
@@ -0,0 +1,11 @@
+var Vector = []
+var N = parseInt(prompt("Cuantos elemntos debe tener el vector"))
+
+for (var i = 0; i<=N; i++){
+    Vector.push(i+1)
+    var x = Vector[i] + Vector[i+1]
+    var count = x
+}
+console.log(Vector)
+console.log(x)
+console.log(count)
