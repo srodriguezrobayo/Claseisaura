ef2fc105a150dfe53578a3b55d86b37c25773443
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2023-09-07 08:40:26 -0500

    Segundo algoritmo evaluacion
    OK

---
+++ Anual.js
@@ -0,0 +1,13 @@
+var Empleado = prompt("Nombre empleado")
+
+var Sueldo = prompt("Cuanto resibe de sueldo")
+
+var Bonificacion = prompt ("De cuanto es la bonificacion mesual del empleado")
+
+var ba = Bonificacion * 12
+
+var sa = Sueldo * 12
+
+var Total = sa + ba
+
+console.log ("El sueldo anual del empleado ",Empleado," es de: ",Total)
