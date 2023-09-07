2f816de6a6b9e80d8c5cbcafbc4fa11816439b90
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2023-09-07 08:27:45 -0500

    Primer algoritmo

---
+++ Empleados.js
@@ -0,0 +1,12 @@
+var Numero 
+
+for (var i = 0; i<=Numero; i++){
+    var Nombre = prompt("Ingrese el nombre del empleado")
+    var Sueldo = parseInt(prompt("Ingrese el sueldo del empleado"))
+    if (Sueldo > 1160000){
+        console.log("Estos empleados Ganan mas del salario promedio:")
+        console.log("Nombre: ".Nombre)
+        console.log("Sueldo: ".Sueldo)
+    }
+}
+
